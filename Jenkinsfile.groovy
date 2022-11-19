ad1b9291f9a9d434f72e2d7b9fe560df2bf40496
saurabh-aggarwal-nbs <saurabh.aggarwal@nationwide.co.uk>
2022-11-19 22:59:15 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -197,3 +197,34 @@ def deployArtifact(component) {
     /// do your deployment step here
     component.deployed = true
 }
+
+
+def checkoutRepository(repository) {
+    def checkoutName = "refs/heads/${repository.branch}"
+    if(repository.tag){
+        checkoutName = "refs/tags/${repository.tag}"
+        println "Checking out from tag ${checkoutName}"
+    }
+    else if(repository.commit)
+    {
+        checkoutName = "${repository.commit}"
+        println "Checking out from commit ${checkoutName}"
+    }
+    else
+    {
+        println "Checking out from branch ${checkoutName}"
+    }
+    println "Repository name : ${repository.name}"
+    def checkoutInfo = checkout([
+            $class: 'GitSCM',
+            branches: [[name: checkoutName]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [],
+            submoduleCfg: [],
+            userRemoteConfigs: [[
+                                        credentialsId: 'saurabh-aggarwal-nbs',
+                                        url: "https://github.com/saurabh-aggarwal-nbs/${repository.name}.git/"
+                                ]]
+    ])
+    return checkoutInfo
+}
