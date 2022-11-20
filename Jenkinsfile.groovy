d5adc0c4bbace0205c2dd48ffdf6bd976baa8fff
saurabh-aggarwal-nbs <jenkins@test.com>
2022-11-20 18:09:27 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -226,37 +226,6 @@ def deployArtifact(component) {
 }
 
 
-def checkoutRepository(repository) {
-    def checkoutName = "refs/heads/${repository.branch}"
-    if(repository.tag){
-        checkoutName = "refs/tags/${repository.tag}"
-        println "Checking out from tag ${checkoutName}"
-    }
-    else if(repository.commit)
-    {
-        checkoutName = "${repository.commit}"
-        println "Checking out from commit ${checkoutName}"
-    }
-    else
-    {
-        println "Checking out from branch ${checkoutName}"
-    }
-    println "Repository name : ${repository.name}"
-    def checkoutInfo = checkout([
-            $class: 'GitSCM',
-            branches: [[name: checkoutName]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [],
-            submoduleCfg: [],
-            userRemoteConfigs: [[
-                                        credentialsId: 'saurabh-aggarwal-nbs',
-                                        url: "https://github.com/saurabh-aggarwal-nbs/${repository.name}.git/"
-                                ]]
-    ])
-    return checkoutInfo
-}
-
-
 def updateBaselineFile(){
     dir("checkoutdir") {
         checkout([$class: 'GitSCM', branches: [[name: 'refs/heads/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
